e39c0bc2bfc01a061bdd4a51f90f19b265af5442
andrew <mykge@outlook.com>
2023-07-25 17:49:55 +0800

    update

--- main.go
+++ main.go
@@ -29,32 +29,39 @@ func main() {
 
 	InitTelebot("")
 
+	Startup()
+
 	select {}
 }
 
 type IncomesMessage struct {
-	Sender   string
-	Date     string
-	Time     string
-	Serial   string
-	Sequence string
-	Type     string //ext
+	// Sender   string
+	// Date     string
+	// Time     string
+	// Serial   string
+	// Sequence string
+	// Type     string //ext
 	FileName string
 }
 
+// 0 IN20230725
+// 1 174425
+// 2 00
+// 3 CMHK
+// 4 00.txt
 func ParseIncomesMessage(s string) (*IncomesMessage, error) {
 	parts := strings.Split(s, "_")
-	if len(parts) != 6 || !strings.HasPrefix(parts[0], "IN") || !strings.Contains(parts[5], ".") {
+	if len(parts) < 5 || !strings.HasPrefix(parts[0], "IN") || !strings.Contains(parts[4], ".") {
 		return nil, fmt.Errorf("invalid message format")
 	}
 
 	return &IncomesMessage{
-		Date:     parts[0][2:],
-		Time:     parts[1],
-		Serial:   parts[2],
-		Sender:   parts[3],
-		Sequence: parts[4],
-		Type:     strings.Split(parts[5], ".")[1],
+		// Date:     parts[0][2:],
+		// Time:     parts[1],
+		// Serial:   parts[2],
+		// Sender:   parts[3],
+		// Sequence: parts[4],
+		// Type:     strings.Split(parts[5], ".")[1],
 		FileName: s,
 	}, nil
 }
@@ -74,13 +81,8 @@ func ResendIncomesMessage(im *IncomesMessage) error {
 		return err
 	}
 	builder := strings.Builder{}
-	builder.WriteString("Sender: ")
-	builder.WriteString(im.Sender)
-	builder.WriteString("\r\n")
-	builder.WriteString("Date: ")
-	builder.WriteString(im.Date)
-	builder.WriteString(" ")
-	builder.WriteString(im.Time)
+	builder.WriteString("FileName: ")
+	builder.WriteString(im.FileName)
 	builder.WriteString("\r\n")
 	builder.WriteString("Content:\r\n")
 	builder.WriteString("====== BEGIN ======\r\n")
@@ -104,6 +106,17 @@ func ResendIncomesMessage(im *IncomesMessage) error {
 	}
 
 	return nil
+}
+
+func Startup() {
+	c := WatchIncomesMessage(context.Background())
+	for {
+		msg := <-c
+		if msg == nil {
+			continue
+		}
+		ResendIncomesMessage(msg)
+	}
 }
 
 func WatchIncomesMessage(ctx context.Context) <-chan *IncomesMessage {
